3ad5b38c0a6e85f0b345b183c85a054073c06bc8
Paul B. Hartzog <paul.hartzog@holo.host>
2018-11-28 12:19:29 -0500

    fixing code with Michael

--- accountant-app/zomes/service/code/src/lib.rs
+++ accountant-app/zomes/service/code/src/lib.rs
@@ -1,22 +1,45 @@
+#![feature(try_from)]
+
 #[macro_use]
 extern crate hdk;
 pub extern crate serde;
+#[macro_use]
 extern crate serde_derive;
+#[macro_use]
 extern crate serde_json;
+#[macro_use]
+extern crate holochain_core_types_derive;
+
+use hdk::{
+    entry_definition::ValidatingEntryType,
+    error::{ZomeApiError, ZomeApiResult},
+    holochain_core_types::{
+        entry::Entry,
+        error::HolochainError,
+        hash::HashString,
+        json::{DefaultJson, JsonString},
+        validation::EntryAction,
+    }
+};
+use serde::Serialize;
+use serde_json::{Value};
 
 // see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
+
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson)]
 pub struct ServiceCycle {
     pub agent_key: String,
     pub request_hash: HashString,
     pub response_hash: HashString,
-    pub metrics: ServiceMetrics,
+    pub metrics: String,
     pub signature: Option<String>,
 }
 
 fn handle_log(log: ServiceCycle) -> serde_json::Value {
-	match hdk::commit_entry("log", json!(ServiceCycle)) {}
+	match hdk::commit_entry("log", json!(ServiceCycle)) {
+
+    }
 }
 
 define_zome! {
@@ -39,14 +62,14 @@ define_zome! {
 
     functions: {
         main (Public) {
-            handle_log: {
+            log: {
                 inputs: |
                     agent_key: String,
                     request: Value,
                     response: Value,
-                    metrics: "none"
+                    metrics: String
                 |,
-                outputs: |result: serde_json::Value|
+                outputs: |result: serde_json::Value|,
                 handler: handle_log
             }
         }
