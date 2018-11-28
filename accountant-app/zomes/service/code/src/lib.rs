63022222ccb050d7f554e8f39117d4e182fce115
Paul B. Hartzog <paul.hartzog@holo.host>
2018-11-28 10:57:53 -0500

    added entry macro

--- accountant-app/zomes/service/code/src/lib.rs
+++ accountant-app/zomes/service/code/src/lib.rs
@@ -10,8 +10,27 @@ pub extern crate holochain_wasm_utils;
 
 // see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
 
+#[derive(Serialize, Deserialize, Debug, DefaultJson)]
+pub struct ServiceCycle {
+    pub agent_key: String,
+    pub request_hash: HashString,
+    pub response_hash: HashString,
+    pub metrics: ServiceMetrics,
+    pub signature: Option<String>,
+}
+
 define_zome! {
     entries: [
+        entry!(
+            name: "service-logger",
+            description: "A log of a single request_payload/response_payload cycle",
+            sharing: Sharing::Public,
+            native_type: ServiceCycle,
+            validation_package: || hdk::ValidationPackageDefinition::Entry,
+            validation: |service_cycle: ServiceCycle, _ctx: hdk::ValidationData| {
+                Ok(())
+            }
+        ),
     ]
 
     genesis: || {
