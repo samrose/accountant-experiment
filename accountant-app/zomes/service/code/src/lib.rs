24ccc8274e20d690266bb6306527c673aab420c1
Paul B. Hartzog <paul.hartzog@holo.host>
2018-11-28 10:46:50 -0500

    added crates

--- accountant-app/zomes/service/code/src/lib.rs
+++ accountant-app/zomes/service/code/src/lib.rs
@@ -1,15 +1,23 @@
 #[macro_use]
 extern crate hdk;
-extern crate serde;
+pub extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
+
+pub extern crate holochain_core_types;
+pub extern crate holochain_dna;
+pub extern crate holochain_wasm_utils;
 
 // see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
 
 define_zome! {
-    entries: []
+    entries: [
+    ]
 
-    genesis: || { Ok(()) }
+    genesis: || {
+        Ok(())
+    }
 
-    functions: {}
+    functions: {
+    }
 }
