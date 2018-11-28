08dd6c481471fd703e7f4b16c69754c1d24b49fe
Paul B. Hartzog <paul.hartzog@holo.host>
2018-11-28 11:05:57 -0500

    update

--- accountant-app/zomes/service/code/src/lib.rs
+++ accountant-app/zomes/service/code/src/lib.rs
@@ -4,10 +4,6 @@ pub extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 
-pub extern crate holochain_core_types;
-pub extern crate holochain_dna;
-pub extern crate holochain_wasm_utils;
-
 // see https://holochain.github.io/rust-api/0.0.1/hdk/ for info on using the hdk library
 
 #[derive(Serialize, Deserialize, Debug, DefaultJson)]
